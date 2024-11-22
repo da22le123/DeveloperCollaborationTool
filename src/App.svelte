789ee9e407c60a1febd9ec5e2ccea1ab76cbf2ae
Kirill Loskutov <555320@student.saxion.nl>
2024-11-22 12:50:51 +0000

    Bootstrap project

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+import router from "page";
+
+import Home from "./pages/Home.svelte";
+import NotFound from "./pages/NotFound.svelte";
+
+let page;
+let params;
+let currentRoute;
+
+router("/", (ctx) => {
+    page = Home;
+    currentRoute = ctx.pathname;
+});
+router("*", (ctx) => {
+    page = NotFound;
+    currentRoute = ctx.pathname;
+});
+
+router.start();
+</script>
+
+<main>
+    <svelte:component this={page} {params} />
+</main>
+
+<style>
+</style>
