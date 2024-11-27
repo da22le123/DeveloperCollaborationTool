002f9b0a9968e66eb54c0ec623df5944c9d1c307
nestorka <nestorgg6@gmail.com>
2024-11-27 20:18:28 +0100

    LoginForm(HTML only)

---
+++ src/components/LoginForm.svelte
@@ -0,0 +1,16 @@
+<div class="wrapper">
+    <div class="login-form">
+        <h2>Login</h2>
+        <form class="login-form">
+            <div class="input-box">
+                <input type="email" required>
+                <label>Email</label>
+            </div>
+
+            <div class="input-box">
+                <input type="password" required>
+                <label>Password</label>
+            </div>
+        </form>
+    </div>
+</div>
