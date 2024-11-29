74c9f6f9b0db5a0f2e482bf970587492fdea0174
nestorka <nestorgg6@gmail.com>
2024-11-29 14:20:47 +0100

    Work in progress

--- src/frontend/src/components/LoginForm.svelte
+++ src/frontend/src/components/LoginForm.svelte
@@ -1,16 +1,22 @@
+<script>
+    let email = "";
+    let password = "";
+</script>
+
 <div class="wrapper">
     <div class="login-form">
         <h2>Login</h2>
         <form class="login-form">
             <div class="input-box">
-                <input type="email" required>
+                <input type="email" bind:value={email} required>
                 <label>Email</label>
             </div>
 
             <div class="input-box">
-                <input type="password" required>
+                <input type="password" bind:value={password} required>
                 <label>Password</label>
             </div>
+            <button type="submit">Login</button>
         </form>
     </div>
 </div>
---
+++ src/frontend/src/pages/LoginPage.svelte
@@ -0,0 +1,11 @@
+<script>
+    import LoginForm from '../components/LoginForm.svelte'
+
+
+</script>
+
+<main>
+    <div>
+        <LoginForm/>
+    </div>
+</main>
