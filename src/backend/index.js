f9a2a51b1b9c4ae341324880ed8aad1fb132bf6d
denys <543499@student.saxion.nl>
2024-11-29 14:22:07 +0100

    Implement global error handling

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import cors from "cors";
 import express from "express";
+import "express-async-errors";
 
 import usersRoute from "./routes/users.js";
 
@@ -9,7 +10,15 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const errorHandling = (err, req, res, next) => {
+    if (err.name === "ValidationError") {
+        return res.status(400).json({ errors: err.errors });
+    }
+    res.status(500).json({ error: "Something went wrong!" });
+};
+
 app.use("/", usersRoute);
+app.use(errorHandling);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
