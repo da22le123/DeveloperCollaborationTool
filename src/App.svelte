2fb7d13ea13080f7e30f914c7a72f708af79b2e4
nestorka <nestorgg6@gmail.com>
2024-12-01 00:44:14 +0100

    Frontend handling of Login completed

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@ import router from "page";
 
 import Home from "./pages/Home.svelte";
 import NotFound from "./pages/NotFound.svelte";
-import LoginPage from "./pages/LoginPage.svelte";
+import Login from "./pages/Login.svelte";
 
 let page;
 let params;
@@ -13,14 +13,15 @@ router("/", (ctx) => {
     page = Home;
     currentRoute = ctx.pathname;
 });
+router('/login', (ctx) => {
+    page = Login;
+    currentRoute = ctx.pathname;
+})
 router("*", (ctx) => {
     page = NotFound;
     currentRoute = ctx.pathname;
 });
-router("/login", (ctx) => {
-    page = LoginPage;
-    currentRoute = ctx.pathname;
-})
+
 
 router.start();
 </script>
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,11 +1,27 @@
 <script>
 import Counter from "../lib/Counter.svelte";
+import {tokenStore, clearToken} from "../stores/tokenStore.js";
+import page from "page";
+
+const logoutTest = () => {
+    clearToken();
+};
+const loginTest = () => {
+    page("/login");
+};
+
 </script>
 
 <main>
     <div class="card bg-amber-500">
         <Counter />
     </div>
+    {#if $tokenStore}
+        <strong>Hello user!!!</strong>
+        <button on:click={clearToken}>Logout test</button>
+        {:else}
+        <button on:click={loginTest}>Login test</button>
+        {/if}
 </main>
 
 <style>
