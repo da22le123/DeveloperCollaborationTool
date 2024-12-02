9f420c6929fb317727e4a0cbd5b62ff8afa3fede
nestorka <nestorgg6@gmail.com>
2024-12-02 17:06:50 +0100

    added store.subscribe and extracted API endpoint to env file

--- src/stores/tokenStore.js
+++ src/stores/tokenStore.js
@@ -1,29 +1,40 @@
 import {parseJwt} from "./parser.js";
 import {writable, derived} from "svelte/store";
 
+
 const storedToken = localStorage.getItem("token");
+
 
 export const tokenStore = writable(storedToken);
 
-export const userStore = derived(tokenStore, (token) => (token ? parseJwt(token) : null));
 
-export const userEmail = derived(tokenStore, (token) => token ? parseJwt(token).email : null)
-
-export const isAdminStore = derived (userStore, (user) => {
-    return user && user.isAdmin;
+tokenStore.subscribe((token) => {
+    if (token) {
+        localStorage.setItem("token", token);
+    } else {
+        localStorage.removeItem("token");
+    }
 });
 
-export const isLeadStore = derived (userStore, (user) => {
-    return user && user.isLead;
-})
+
+export const userStore = derived(tokenStore, (token) => (token ? parseJwt(token) : null));
+
+
+export const userEmail = derived(tokenStore, (token) =>
+    token ? parseJwt(token).email : null
+);
+
+
+export const isAdminStore = derived(userStore, (user) => user && user.isAdmin);
+
+
+export const isLeadStore = derived(userStore, (user) => user && user.isLead);
 
 
 export function setToken(token) {
-    localStorage.setItem("token", token);
     tokenStore.set(token);
 }
 
 export function clearToken() {
-    localStorage.removeItem("token");
     tokenStore.set(null);
 }
