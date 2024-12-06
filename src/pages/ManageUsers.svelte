acff839428550df5befdbe1cdc045b49c8fc9d78
Yevheniia Monashko <550003@student.saxion.nl>
2024-12-06 14:33:33 +0000

    Create user  list page

---
+++ src/pages/ManageUsers.svelte
@@ -0,0 +1,73 @@
+<script>
+import page from "page";
+import { isAdminStore } from "../stores/tokenStore.js";
+import { tokenStore } from "../stores/tokenStore";
+
+if (!$tokenStore || !$isAdminStore) {
+    page("/login");
+}
+
+async function fetchUsers() {
+    const response = await fetch("http://localhost:3000/users", {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${$tokenStore}`,
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch users: ${response.statusText}`);
+    }
+
+    return response.json();
+}
+
+const userPromise = fetchUsers();
+</script>
+
+<div class="flex flex-col items-center  min-h-screen py-10">
+    <div class="w-full max-w-5xl mb-6">
+        <h1 class="text-2xl font-bold text-gray-800 mt-10 mb-10">Manage Users</h1>
+    </div>
+    <div class="w-full max-w-5xl bg-white  overflow-hidden ">
+        {#await userPromise}
+            <div class="text-center py-10">Loading users...</div>
+        {:then users}
+            {#if users.length > 0}
+                <div class="w-full max-w-5xl mb-2">
+                    <p class="text-lg  text-left font-bold text-gray-800">All Members</p>
+                </div>
+                <table class="table-auto w-full">
+                    <thead class="border-b border-gray-300">
+                    <tr>
+                        <th class=" px-6 py-3 text-center text-stone-400 font-normal tracking-wider">Email</th>
+                        <th class="px-6 py-3 text-center  text-stone-400  font-normal tracking-wider">Name</th>
+                        <th class="px-6 py-3 text-center text-stone-400 font-normal tracking-wider">Role</th>
+                        <th class="px-10 py-3 text-center text-stone-400 font-normal tracking-wider"></th>
+                        <th class="px-6 py-3 text-center text-stone-400 font-normal tracking-wider ml-10">Is Lead</th>
+                        <th class="px-6 py-3 text-center text-stone-400 font-normal tracking-wider">Delete User</th>
+                    </tr>
+                    </thead>
+
+                    <tbody class="divide-y divide-gray-300 ">
+                    {#each users as user (user.id)}
+                        <tr class="hover:bg-gray-100">
+                            <td class="px-6 py-4 text-gray-800 text-left">{user.email}</td>
+                            <td class="px-6 py-4 text-gray-800 text-left">{user.username}</td>
+                            <td class="px-6 py-4 text-gray-800 text-left">{user.role}</td>
+                        </tr>
+                    {/each}
+                    <tr>
+                        <td colspan="5" class="border-t border-gray-300"></td>
+                    </tr>
+                    </tbody>
+                </table>
+            {:else}
+                <div class="text-center text-neutral-400 font-bold py-10">No users found.</div>
+            {/if}
+        {:catch error}
+              <p> An error occurred: {error.message}</p>
+        {/await}
+    </div>
+</div>
+
