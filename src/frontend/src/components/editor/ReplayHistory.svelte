bf41853aba37842b20daf1b328ab29fcd87e2721
Jan Lupčík <557377@student.saxion.nl>
2024-12-12 22:46:33 +0100

    (frontend) Add adding new nodes to the editor.

--- src/frontend/src/components/editor/ReplayHistory.svelte
+++ src/frontend/src/components/editor/ReplayHistory.svelte
@@ -10,7 +10,7 @@ export let open = false;
 const onCloseClick = () => eventDispatcher("closed");
 </script>
 
-<div class="replay-history fixed right-0 top-0 w-80 h-full py-7 bg-gray-100 transition-all" class:closed={!open}>
+<div class="replay-history fixed right-0 top-0 w-80 h-full py-7 bg-gray-100 transition-all z-10" class:closed={!open}>
     <div class="absolute top-2 right-1 px-5 text-xl font-bold cursor-pointer" on:click={onCloseClick}>&#x2715;</div>
 
     <h2 class="text-2xl px-5 font-medium mb-9">Replay History</h2>
--- src/frontend/src/pages/Session.svelte
+++ src/frontend/src/pages/Session.svelte
@@ -4,6 +4,7 @@ import { SvelteFlowProvider } from "@xyflow/svelte";
 import Editor from "../components/Editor.svelte";
 import ReplayHistory from "../components/editor/ReplayHistory.svelte";
 import ObjectList from "../components/editor/ObjectList.svelte";
+import EditorObjectProvider from "../providers/EditorObjectProvider.svelte";
 
 export let params;
 
@@ -15,29 +16,31 @@ const onToggleHistory = () => {
 </script>
 
 <SvelteFlowProvider>
-    <div class="flex w-full">
-        <div class="w-96 pr-10">
-            <h1 class="text-2xl font-semibold mb-6">Session Name</h1>
+    <EditorObjectProvider>
+        <div class="flex w-full">
+            <div class="w-96 pr-10">
+                <h1 class="text-2xl font-semibold mb-6">Session Name</h1>
 
-            <div class="flex justify-between mb-6">
-                <button class="btn-black px-7">Export</button>
-                <button class="btn-black px-9">Invite members</button>
+                <div class="flex justify-between mb-6">
+                    <button class="btn-black px-7">Export</button>
+                    <button class="btn-black px-9">Invite members</button>
+                </div>
+
+                <ObjectList />
+
+                <div class="flex justify-between">
+                    <button class="btn-red px-4">Explore Statistics</button>
+                    <button class="btn-black px-7" on:click={onToggleHistory}>
+                        {activeHistory ? 'Close History' : 'View History'}
+                    </button>
+                </div>
             </div>
 
-            <ObjectList />
-
-            <div class="flex justify-between">
-                <button class="btn-red px-4">Explore Statistics</button>
-                <button class="btn-black px-7" on:click={onToggleHistory}>
-                    {activeHistory ? 'Close History' : 'View History'}
-                </button>
+            <div class="flex-auto border-black border-2">
+                <Editor />
             </div>
         </div>
-
-        <div class="flex-auto border-black border-2">
-            <Editor />
-        </div>
-    </div>
+    </EditorObjectProvider>
 </SvelteFlowProvider>
 
 <ReplayHistory open={activeHistory} on:closed={onToggleHistory} />
