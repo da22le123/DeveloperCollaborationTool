f9feae21f69ab2da3d6ab135fc850003ea71b7c2
Jan Lupčík <557377@student.saxion.nl>
2024-12-15 23:24:25 +0100

    (frontend) Add a context menu for editing nodes.

---
+++ src/components/editor/NodeContextMenu.svelte
@@ -0,0 +1,66 @@
+<script>
+import { useNodes } from "@xyflow/svelte";
+
+import ContextMenu from "./ContextMenu.svelte";
+
+const nodes = useNodes();
+
+export let node;
+export let position;
+
+const updateNode = ({ label, backgroundColor }) => {
+    if (!node) {
+        return;
+    }
+
+    for (const entry of $nodes) {
+        if (entry.id === node.id) {
+            // Important: the data object has to be reassigned, otherwise, the node will not be updated!
+            entry.data = { ...entry.data, label, backgroundColor };
+
+            entry.style = [`background-color: ${backgroundColor}`].join(";");
+        }
+    }
+
+    $nodes = [...$nodes];
+};
+
+// There are various problems with handling the data variables:
+// 1. Turning the local variables into states with two-way binding on them breaks editing.
+// 2. Using states to handle the input event manually causes an error.
+// 3. Using local variables without the effect below does not load the correct node data
+// when opening the context menu for a different node.
+// 4. Replacing the input events with two-way binding breaks editing (same as 1).
+//
+// Therefore, even though the effect of watching changes in the context and manually
+// handling input events does not look the prettiest, it is the only solution.
+//
+// There is still a minor issue with the color being retained when switching between
+// nodes. However, any attempts to fix this causes one of the issues mentioned above.
+
+let label;
+let color;
+
+$: {
+    label = node?.data?.label;
+    color = node?.data?.backgroundColor || "#FFFFFF";
+}
+
+$: updateNode({ label, backgroundColor: color });
+</script>
+
+<ContextMenu position={position} hide={!node}>
+    {#if node && "label" in node.data}
+        <input type="text"
+               class="block mb-2"
+               value={node.data.label}
+               on:input={(event) => label = event.target.value} />
+    {/if}
+
+    {#if node}
+        <input type="color"
+               class="block w-full h-6"
+               value={node.data.backgroundColor || "#FFFFFF"}
+               on:input={(event) => color = event.target.value} />
+    {/if}
+</ContextMenu>
