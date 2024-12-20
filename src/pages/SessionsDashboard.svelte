bc91515a0c3492745f7443b7c9dd6f1b592b298f
Illia Pavelko <547534@student.saxion.nl>
2024-12-20 12:57:55 +0000

    Change session status frontend

--- src/pages/SessionsDashboard.svelte
+++ src/pages/SessionsDashboard.svelte
@@ -12,7 +12,7 @@ const sessionsPromise = get("/users/sessions");
     {:then sessions}
         <div class="grid grid-cols-3">
         {#each sessions as session}
-            <SessionCard title = {session.name} state = {session.is_open ? 'Open' : 'Closed'} />
+            <SessionCard title = {session.name} state = {session.is_open ? 'Open' : 'Closed'} id = {session.id}/>
         {/each}
         </div>
     {:catch error}
