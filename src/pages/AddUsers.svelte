25158a118e6a5c99ded7be46cd0c9c1c2dd9193e
da22le123 <ilia1892006@gmail.com>
2025-01-16 18:19:07 +0100

    resolve change requests

--- src/pages/AddUsers.svelte
+++ src/pages/AddUsers.svelte
@@ -1,63 +1,38 @@
 <script>
-import page from "page";
-import { isAdminStore } from "../stores/tokenStore.js";
-import { tokenStore } from "../stores/tokenStore";
-import { showPopupMessage } from "../stores/popupStore";
-import { request } from "../utils/fetch.js";
+import { showPopupMessage } from "../stores/popupStore.js";
+import { request, get } from "../utils/fetch.js";
 
 export let params;
-const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
-let users = [];
-let message = "";
-let type = "";
-let isVisible = false;
+const sessionId = params?.params?.id;
 
-let session_id = params?.params?.id;
-
-if (!$tokenStore || !$isAdminStore) {
-    page("/login");
-}
+let usersPromise = fetchUsersAndInviteStatuses();
 
 async function fetchUsersAndInviteStatuses() {
-    const response = await fetch(`${baseUrl}/sessions/${session_id}/users`, {
-        method: "GET",
-        headers: {
-            Authorization: `Bearer ${$tokenStore}`,
-        },
-    });
-
-    if (!response.ok) {
-        throw new Error(`Failed to fetch users: ${response.statusText}`);
-    }
-
-    users = await response.json();
+    return await get(`/sessions/${sessionId}/users`);
 }
 
 async function inviteUser(userId) {
     try {
-        const response = await request(`/sessions/${session_id}/members`, {
+        await request(`/sessions/${sessionId}/members`, {
             user_id: userId,
         });
         // Update the local `users` array to reflect the new state
-        const userIndex = users.findIndex((user) => user.id === userId);
-        if (userIndex !== -1) {
-            users[userIndex].isMember = true;
-        }
+        usersPromise = fetchUsersAndInviteStatuses();
         showPopupMessage("User invited successfully", "success", 3000);
     } catch (error) {
         showPopupMessage("Failed to invite user", "error", 3000);
     }
 }
-
-const userPromise = fetchUsersAndInviteStatuses();
 </script>
 <div class="flex flex-col items-center  min-h-screen py-10">
     <div class="w-full max-w-5xl mb-6 text-center">
         <h1 class="text-2xl font-bold text-gray-800 mt-10 mb-10">Bring your team on board</h1>
     </div>
     <div class="w-full max-w-5xl bg-white  overflow-hidden ">
-        {#if users.length > 0}
+        {#await usersPromise}
+            <div class="text-center text-neutral-400 font-bold py-10">Loading...</div>
+        {:then users}
             <div class="w-full max-w-5xl mb-2">
                 <p class="text-lg  text-left font-bold text-gray-800">All Members</p>
             </div>
@@ -79,7 +54,7 @@ const userPromise = fetchUsersAndInviteStatuses();
                         <td class="px-6 py-4 text-gray-800 text-center">{user.role}</td>
                         <td class="px-6 py-4 text-center">
                             {#if user.isMember}
-                                <span class="text-red-500 font-semibold">Invited</span>
+                                <span class="text-black-500 font-semibold">Invited</span>
                             {:else}
                                 <button
                                         class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
@@ -96,9 +71,9 @@ const userPromise = fetchUsersAndInviteStatuses();
                 </tr>
                 </tbody>
             </table>
-        {:else}
-            <div class="text-center text-neutral-400 font-bold py-10">No users found.</div>
-        {/if}
+        {:catch error}
+            <div class="text-center text-neutral-400 font-bold py-10">Failed to fetch users</div>
+        {/await}
     </div>
 </div>
 
