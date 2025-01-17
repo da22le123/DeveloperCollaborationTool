9dc6ea30401297f7168e2cb0b81102a406152dde
nestorka <nestorgg6@gmail.com>
2025-01-17 15:26:11 +0100

    lint

--- src/components/BarChart.svelte
+++ src/components/BarChart.svelte
@@ -1,38 +1,39 @@
 <script>
-    import { Bar } from 'svelte-chartjs';
-    import 'chart.js/auto';
+import { Bar } from "svelte-chartjs";
+import "chart.js/auto";
 
-    export let usernames;
-    export let additionsByUser;
-    export let modificationsByUser;
-    export let deletionsByUser;
+export let usernames;
+export let additionsByUser;
+export let modificationsByUser;
+export let deletionsByUser;
 
 const labels = usernames;
 const data = {
     labels: labels,
     datasets: [
         {
-            label: 'Added',
+            label: "Added",
             data: additionsByUser,
-            backgroundColor: 'green',
-            borderColor: 'green',
+            backgroundColor: "green",
+            borderColor: "green",
             borderWidth: 1,
         },
         {
-            label: 'Updated',
+            label: "Updated",
             data: modificationsByUser,
-            backgroundColor: 'blue',
-            borderColor: 'blue',
+            backgroundColor: "blue",
+            borderColor: "blue",
             borderWidth: 1,
         },
         {
-            label: 'Deleted',
+            label: "Deleted",
             data: deletionsByUser,
-            backgroundColor: 'red',
-            borderColor: 'red',
+            backgroundColor: "red",
+            borderColor: "red",
             borderWidth: 1,
         },
-    ]
-};</script>
+    ],
+};
+</script>
 
 <Bar data={data} width={300} height={300} options={{maintainAspectRatio: false}} />
--- src/components/PieChart.svelte
+++ src/components/PieChart.svelte
@@ -1,21 +1,20 @@
 <script>
-    import { Pie } from 'svelte-chartjs';
-    import 'chart.js/auto';
+import { Pie } from "svelte-chartjs";
+import "chart.js/auto";
 
+export let usernames = [];
+export let userActions = [];
 
-    export let usernames = [];
-    export let userActions = [];
-
-    const chartData = {
-        labels: usernames,
-        datasets: [
-            {
-                label: 'User Actions',
-                data: userActions,
-                borderWidth: 1,
-            },
-        ],
-    };
+const chartData = {
+    labels: usernames,
+    datasets: [
+        {
+            label: "User Actions",
+            data: userActions,
+            borderWidth: 1,
+        },
+    ],
+};
 </script>
 
 <Pie data={chartData} width={300} height={300} options={{ maintainAspectRatio: false }} />
